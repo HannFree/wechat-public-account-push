f8151d852fb1b2e2cd03f3b16bbbc5e9600890dd
HannFree <115334749+HannFree@users.noreply.github.com>
2022-10-08 14:06:57 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'olp3a52n9ShH8HHGmv-yZLszh984',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '4w6bMQAMsQbiJvc9I0foomn1wmOi8Xum4Son9tfTBhY',
+      useTemplateId: 'GYmlbtElTxZxoMlCtXemQttGFC1_TV40fgtosK9VHaw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-01',
       festivals: [
