23292d4728ac006e466639b808d128215f1fc472
HannFree <115334749+HannFree@users.noreply.github.com>
2022-10-08 12:51:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,24 +8,24 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx2456225ce26cb7da',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'f7a944019637c681ee56f73b187be687',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '河北',
+  CITY: '秦皇岛',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '李烁',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'olp3a52n9ShH8HHGmv-yZLszh984',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'OqNvIOEtY8l5LCsJCFLU_YTA-JjN4mXt70nhPL4OjwQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '10-01',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
